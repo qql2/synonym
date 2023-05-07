9f4ef3416ce95d4bd941019ea4854aecd73b620e
qql2 <2482076557@qq.com>
2023-05-07 22:44:25 +0800

    优化代码

--- src/settingTab.ts
+++ src/settingTab.ts
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prefer-const */
 
-import { App, PluginSettingTab } from "obsidian";
-
-import Synonym from '../main';
+import { App, Plugin, PluginSettingTab } from "obsidian";
 
 export class SettingTab extends PluginSettingTab {
-    plugin: Synonym;
+    plugin: Plugin;
     static settingAdders: ((containerEl: HTMLElement) => HTMLElement)[] = [];
-    constructor(app: App, plugin: Synonym) {
+    constructor(app: App, plugin: Plugin) {
         super(app, plugin);
         this.plugin = plugin;
     }
